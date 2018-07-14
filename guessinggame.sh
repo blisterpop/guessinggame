0bcdbe958a22c6fe002480637e484e124f1eeb3a
blisterpop <32981435+blisterpop@users.noreply.github.com>
2018-07-14 13:59:42 -0400

    commit after shell script is finished and has been tested

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
+#function to handle user input and handle null value
+function userread {
+    read GUESS
+    if [[ GUESS -eq 0 ]]
+    then echo "Please type a number.  Blanks are not allowed!"
+    read GUESS 
+    fi 
+}
+
+#determine how many files are in the directory
 ANSWER="$(ls -p | grep -v / | wc -l)"
-echo "${ANSWER}"
+
+#input request (obtain GUESS variable)
+Echo "Guess how many files are in this directory.  How many?"
+userread
+
+#loop to determine if user guess matches actual answer
+while [[ ${ANSWER} -ne ${GUESS} ]]
+do
+    echo "${GUESS} is incorrect. Please try again."
+    userread
+done 
+
+#only runs if user guesses correctly
+    echo "${GUESS} is the correct number! CONGRATULATIONS!" 
+
+#done 
+
+
